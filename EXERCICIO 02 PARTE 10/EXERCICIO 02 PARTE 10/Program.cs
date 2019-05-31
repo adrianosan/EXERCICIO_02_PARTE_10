f111b9bd6853cd33a84989e7924e4b0ae6e2503c
adriano.santos <adriano.santos@squadra.com.br>
2019-05-31 13:26:18 -0300

    exercicio 02 parte 10

---
+++ EXERCICIO 02 PARTE 10/EXERCICIO 02 PARTE 10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_02_PARTE_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            Console.Write("Digite um valor para A.");
+
+            A = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um valor para B.");
+
+            B = int.Parse(Console.ReadLine());
+
+            if (A > B)
+            {
+                Console.Write(A + "é maior que +" + B);
+            }
+            else
+                Console.Write(B + " é maior que +" + B);
+            {
+
+                
+            }
+
+        }
+    }
+}
+            
+     
